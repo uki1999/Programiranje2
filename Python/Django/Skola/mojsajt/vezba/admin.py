92273cffa1caf97e8bc4e404d475b232d94a70ea
uki1999 <urosradisavljevic10@gmail.com>
2021-07-06 02:07:18 +0200

    Poruka 5

--- Python/Django/Skola/mojsajt/vezba/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Vezbe,Vezba
-
-
-admin.site.register(Vezbe)
-admin.site.register(Vezba)
--- Python/Django/Skola/mojsajt/vezba/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VezbaConfig(AppConfig):
-    name = 'vezba'
